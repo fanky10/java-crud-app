e293f3d3215d53f3ed9d9961a1195ae8d4e6ba14
Facundo Scoccia <fanky10@gmail.com>
2016-11-01 13:06:07 -0300

    Initial Project Structure

---
+++ src/main/java/ar/edu/utn/frro/java/web/cwa/HomeServlet.java
@@ -0,0 +1,33 @@
+package ar.edu.utn.frro.java.web.cwa;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HomeServlet
+ */
+public class HomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("welcomeMessage", "Bienvenido internauta!");
+		request.setAttribute("homeTitle", "CRUD PoC");
+		getServletContext().getRequestDispatcher("/WEB-INF/pages/home.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
